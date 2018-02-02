479144d18c8436f17fec9daa47b088efd604e428
Heartbroken-Git <corentin196@gmail.com>
2018-02-02 09:21:14 +0100

    initial commit

---
+++ userlib/.libnachos.d
@@ -0,0 +1,5 @@
+libnachos.o .libnachos.d : libnachos.c libnachos.h ../userlib/syscall.h \
+ ../kernel/copyright.h \
+ /root/Downloads/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdarg.h \
+ /root/Downloads/cross-mips/bin/../lib/gcc/mips/4.5.1/include/stdint.h \
+ /root/Downloads/cross-mips/bin/../lib/gcc/mips/4.5.1/../../../../mips/include/stdint.h
